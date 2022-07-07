c89997108969d8cf8091eaa1973b3b835774c7ad
DanielSalas11 <daniel_salas11@outlook.com>
2022-07-06 23:22:19 -0700

    We made a people counter button which displays the times clicked in a text view.

--- app/src/main/java/daniel/salas/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/daniel/salas/firstkotlinapp/MainActivity.kt
@@ -2,10 +2,25 @@ package daniel.salas.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCLickMe = findViewById<Button>(R.id.myButton)
+        val tvCounter = findViewById<TextView>(R.id.textView)
+        var num = 0
+
+        btnCLickMe.text = "Count People!"
+        btnCLickMe.setOnClickListener {
+            num += 1
+            tvCounter.text = num.toString()
+            Toast.makeText(this,"And another one", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
